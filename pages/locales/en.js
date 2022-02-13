89fe4821b43c84c3659b7d9b82e80fb47ed62500
liranneta23 <netaliran23@gmail.com>
2022-02-13 13:27:01 +0100

    Set languages options

---
+++ pages/locales/en.js
@@ -0,0 +1,12 @@
+export default {
+    hero: 'Access your local community in Clik',
+    subtitle: 'Clik lets you benefit and contribute to your locality - Stay connected!',
+    waitlistbutton: 'Join Wait List',
+    soonavailable: 'Clik will soon be available, join wait list to stay informed',
+    home: 'Home',
+    why: 'Why',
+    contact: 'Contact',
+    beta: 'Beta',
+    faq: 'FAQ',
+    language: 'Language',
+}
