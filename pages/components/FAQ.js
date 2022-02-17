ff19b5383da392e28fca9b7559b536690a0ff146
liranneta23 <netaliran23@gmail.com>
2022-02-17 12:37:29 +0100

    Update FAQs

--- pages/components/FAQ.js
+++ pages/components/FAQ.js
@@ -10,26 +10,31 @@ const FAQ = () => {
           <Accordion.Item eventKey="0">
             <Accordion.Header>What is Clik?</Accordion.Header>
             <Accordion.Body>
-              Clik is a community of people that let&apos;s you benefit from and
-              add to the strengths of your local community via the marketplace,
-              events, services, volunteering, donations, jobs, and accomodation
-              listings.
+            Clik is a community driven platform that initiates social interactivity by keeping you updated 
+            and connected to the happenings in your locality. By highlighting what matters to you, you won&apos;t
+            miss out on important events, marketplace and job listings and much more.
+            Do you want to see changes or seek support for your ideas in your neighborhood? 
+            Let your voice be heard in the local feedback feed. 
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="1">
             <Accordion.Header>How does Clik work?</Accordion.Header>
             <Accordion.Body>
-              Clik relies on true citizens of the world like you, who register
-              on the platform to share updates such as a product listing to
-              operate.
+            Clik operates on a simple principle: The more people use the app, 
+            the more activities and items will be displayed on the main feed.
+            The mainfeed is customizable based on user preferences.
+            Thus, Clik relies on citizens like you, to update respective listings to the app.
+            The more input, the more relevant the activities and items!
+
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="2">
             <Accordion.Header>How can Clik help you?</Accordion.Header>
             <Accordion.Body>
-              Clik serves as a viable medium that let&apos;s you share your
-              thoughts about your community in the local feedback feed, promote
-              an event, job offers, services and donations in the feed.
+            Socializing should not require hard effort. Finding out about your community should not result in 
+            full blown research. Therefore, Clik is the medium that seamlessly allows you to connect to individuals, 
+            resources and activities within your community. 
+
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
