ee3e2c74c95cebba4c2c1d260c368f9102bf6788
Liran Neta <netaliran23@gmail.com>
2022-08-01 16:16:07 +0200

    change subtitle bold header section

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -34,7 +34,7 @@ const Header = () => {
                 <strong>{t.hero}</strong>
               </h1>
               <p>
-                {t.subtitle}                
+                On Clik you can find out the events in Enschede that <strong>matter to you</strong> - Stay connected!
               </p>
 
               <OurForm />
