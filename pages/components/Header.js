ebeb48742757662337d52429e19618ee7f0dcae5
Liran Neta <netaliran23@gmail.com>
2023-05-10 23:08:35 +0200

    remove margin left

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -96,7 +96,7 @@ const Header = () => {
             </div>
           </Col>
           <Col md={(6, { order: "last" })} xs={(12, { order: "first" })}>
-            <div data-aos="fade-left" style={{ marginLeft: '20px' }}>
+            <div data-aos="fade-left">
               <div className="justify-content-center">
                 {/* <video src={appData.Clik-Feeda}></video> */}
                 <video width={250} muted autoPlay loop>
