09a5d2bcfdb6fb595fd90fc110873ad68db3fae7
Liran Neta <netaliran23@gmail.com>
2023-05-10 23:27:22 +0200

    add the option for webm video, if mp4 doesnt work

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -100,6 +100,7 @@ const Header = () => {
               <div className="justify-content-center">
                 <video style={{maxWidth: "300px", width: "100%"}} muted autoPlay loop>
                   <source src="/Clik-Feed-ReactNative.mp4" type="video/mp4" />
+                  <source src="/Clik-Feed-ReactNative.webm" type="video/webm" />
                   <p>
                     Your browser doesnt support HTML video. Here is a
                     <a href="/Clik-Feed-ReactNative.mp4">link to the app demonstration</a> instead.
